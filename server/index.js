4b169bb4ebf5803dba5fd2110e9c19a0d4bbdd45
David Chege <dkc.ushift@gmail.com>
2020-10-13 17:47:57 -0500

    Create Basic Proxy

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+const port = 3000;
+const ip = "localhost";
+app.use(morgan('dev'));
+
+app.get('/productinfo', (req, res) => {
+    res.send('This is a proxy service which proxies to Product Information Services API');
+ });
+
+const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+
+
+app.use('/json_placeholder', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/json_placeholder`]: '',
+    },
+ }));
+
+ app.listen(port, ip, () => {
+    console.log(`Starting Proxy at ${ip}:${port}`);
+ });
