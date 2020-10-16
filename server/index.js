0d6160733b224883735095a0627e8fd2ba738f2e
David Chege <dkc.ushift@gmail.com>
2020-10-15 19:31:15 -0500

    Fixing Proxy

--- server/index.js
+++ server/index.js
@@ -6,21 +6,44 @@ const port = 3000;
 const ip = "localhost";
 app.use(morgan('dev'));
 
-app.get('/product', (req, res) => {
-    res.send('This is a proxy service which proxies to Product Information Services API');
+const endpoint = '/api/products/:product_id';
+
+const API_PRODUCT_INFO = "http://localhost:3004";
+const API_REVIEWS = "http://localhost:3001";
+const API_IMAGES= "http://localhost:3003";
+const API_SELLER = "http://localhost:3002";
+
+
+const productInfo = createProxyMiddleware({
+    target: API_PRODUCT_INFO + endpoint,
+    changeOrigin: true,  
  });
 
-const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+ const reviews = createProxyMiddleware({
+    target: API_REVIEWS + endpoint,
+    changeOrigin: true,  
+ });
 
+ const images = createProxyMiddleware({
+    target: API_IMAGES + endpoint,
+    changeOrigin: true,  
+ });
 
-app.use('/', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/json_placeholder`]: '',
-    },
- }));
+ const seller = createProxyMiddleware({
+    target: API_SELLER + endpoint,
+    changeOrigin: true,  
+ });
+
+ app.use("/reviews", reviews);
+ app.use("/seller", seller);
+ app.use("/images", images);
+ app.use("/productInfo", productInfo);
+ 
 
  app.listen(port, ip, () => {
     console.log(`Starting Proxy at ${ip}:${port}`);
  });
+
+ // pathRewrite: {
+//     "/api/products/": '',
+// },
