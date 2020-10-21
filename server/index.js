315c9335744f1b818f6bea122353f0486b2b95f9
David Chege <dkc.ushift@gmail.com>
2020-10-20 19:03:53 -0500

    Proxy Rendering My Service Only

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
 const express = require('express');
-const morgan = require("morgan");
+const morgan = require("morgan")
+const path = require("path");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 const ip = "localhost";
 app.use(morgan('dev'));
+// app.use('/products/:product_id', express.static(path.join(__dirname, './client/dist/dist')))
 
-const endpoint = '/api/products/';
+// const endpoint = '/products/:product_id';
 
-const API_PRODUCT_INFO = "http://localhost:3004";
+// const API_PRODUCT_INFO = "http://localhost:3004";
 // const API_REVIEWS = "http://localhost:3001";
 // const API_IMAGES= "http://localhost:3003";
 // const API_SELLER = "http://localhost:3002";
 
 
-const productInfo = createProxyMiddleware({
-    target: API_PRODUCT_INFO + endpoint,
-    changeOrigin: true,  
- });
+// const productInfo = createProxyMiddleware({
+//     target: API_PRODUCT_INFO + endpoint,
+//     changeOrigin: true,  
+//  });
 
 //  const reviews = createProxyMiddleware({
 //     target: API_REVIEWS + endpoint,
@@ -37,7 +39,12 @@ const productInfo = createProxyMiddleware({
 //  app.use("/reviews", reviews);
 //  app.use("/seller", seller);
 //  app.use("/images", images);
- app.use("/productInfo/", productInfo);
+//  app.use("/:product_id", productInfo);
+ app.use('/', express.static(path.join(__dirname, './../client/dist')))
+ app.use('/products/:product_id', express.static(path.join(__dirname, './../client/dist/index.html')));
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, './../client/dist/index.html'));
+ });
  
 
  app.listen(port, ip, () => {
